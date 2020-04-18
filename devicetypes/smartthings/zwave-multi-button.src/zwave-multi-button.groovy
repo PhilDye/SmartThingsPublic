d96d6f1ac2115e8d244f1e688c8837aadf1a3c88
Phil Dye <phil@phildye.org>
2020-04-18 12:39:34 +0100

    Add US version (untested without one)

--- devicetypes/smartthings/zwave-multi-button.src/zwave-multi-button.groovy
+++ devicetypes/smartthings/zwave-multi-button.src/zwave-multi-button.groovy
@@ -33,6 +33,7 @@ metadata {
 		fingerprint mfr: "0086", prod: "0001", model: "0058", deviceJoinName: "Aeotec KeyFob", mnmn: "SmartThings", vid: "generic-4-button" //EU
 		fingerprint mfr: "010F", prod: "1001", model: "3000", deviceJoinName: "Fibaro KeyFob", mnmn: "SmartThings", vid: "generic-6-button" //AU
 		fingerprint mfr: "0208", prod: "0200", model: "000B", deviceJoinName: "Hank Four-key Scene Controller", mnmn: "SmartThings", vid: "generic-4-button" //EU
+		fingerprint mfr: "0208", prod: "0201", model: "000B", deviceJoinName: "Hank Four-key Scene Controller", mnmn: "SmartThings", vid: "generic-4-button" //US
 	}
 
 	tiles(scale: 2) {
@@ -217,7 +218,8 @@ private getProdNumberOfButtons() {[
 		"0002" : 4,
 		"0101" : 4,
 		"0001" : 4,
-		"0200" : 4
+		"0200" : 4,
+		"0201" : 4
 ]}
 
 private getSupportedButtonValues() {
